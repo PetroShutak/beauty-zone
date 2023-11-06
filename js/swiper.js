4010ccbec5ea4287adbb012e5070f012c055aaf3
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-11-06 23:00:10 +0200

    fix custom button for swiper

--- js/swiper.js
+++ js/swiper.js
@@ -4,7 +4,12 @@ const swiper = new Swiper('.swiper', {
   loop: true,
   slidesPerView: 3,
   spaceBetween: 30,
-  
+  navigation: {
+    nextEl: '#custom-next-button',
+    prevEl: '#custom-prev-button',
+    clickable: true,
+  },
+
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
@@ -12,14 +17,22 @@ const swiper = new Swiper('.swiper', {
   },
 
   // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    clickable: true,
-  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  //   clickable: true,
+  // },
 
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+});
+
+document.getElementById('custom-prev-button').addEventListener('click', () => {
+  swiper.slidePrev();
+});
+
+document.getElementById('custom-next-button').addEventListener('click', () => {
+  swiper.slideNext();
 });
