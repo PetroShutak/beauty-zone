c3d71fd417a29b80e69f63eac661705b52645caa
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-11-08 00:49:44 +0200

    add modal

--- js/modal.js
+++ js/modal.js
@@ -1,23 +1,38 @@
-const swiper = new Swiper('.swiper-container', {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 20,
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-    },
-    // when window width is >= 640px
-    1280: {
-      slidesPerView: 3,
-    }
-  },
-  pagination: {
-    el: '.pagination',
-    bulletClass: 'pagination__button',
-    bulletActiveClass: 'pagination__button--active',
-  },
-  navigation: {
-    nextEl: '.carousel-button.next',
-    prevEl: '.carousel-button.prev',
-  },
+const openModalBtn = document.getElementById('openModal');
+const modal = document.getElementById('modal');
+const closeModalBtn = document.getElementById('closeModal');
+const submitButton = document.getElementById('submitButton');
+
+function openModal() {
+  modal.style.display = 'block';
+  document.body.classList.add('modal-open');
+  loader.style.display = 'none';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+  document.body.classList.remove('modal-open');
+}
+
+openModalBtn.addEventListener('click', openModal);
+
+closeModalBtn.addEventListener('click', closeModal);
+
+window.addEventListener('click', event => {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});
+
+submitButton.addEventListener('click', () => {
+  loader.style.display = 'block';
+  setTimeout(() => {
+    closeModal();
+  }, 2000);
 });
