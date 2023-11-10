4024f091cfc518f2eb116d045802290395342b3a
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-11-10 23:07:27 +0200

    burger menu

--- js/modal.js
+++ js/modal.js
@@ -29,3 +29,29 @@ submitButton.addEventListener('click', () => {
     toggleModal();
   }, 2000);
 });
+
+// burger menu
+
+const burger = document.querySelector('.burger');
+const burgerClose = document.querySelector('.burger_close');
+const burgerMenu = document.querySelector('.burger_modal_container');
+const navBurger = document.querySelector('.burger_nav');
+
+const toggleBurger = () => {
+  burgerMenu.classList.toggle('modal__hidden');
+  document.body.classList.toggle('modal-open');
+};
+
+const hideBurgerMenu = () => {
+  burgerMenu.classList.add('modal__hidden');
+  document.body.classList.remove('modal-open');
+};
+
+burger.addEventListener('click', toggleBurger);
+burgerClose.addEventListener('click', toggleBurger);
+
+const burgerLinks = document.querySelectorAll('.burger_nav a');
+
+burgerLinks.forEach(link => {
+  link.addEventListener('click', hideBurgerMenu);
+});
